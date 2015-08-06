514f162cbde9622566185ecf27f1341d3abd2151
scagnetti <scagnetti@gmx.net>
2015-08-06 22:30:12 +0200

    Removed fake chat massages due to probable missing user accounts when initializing the app

--- chat.js
+++ chat.js
@@ -25,9 +25,9 @@ if (Meteor.isClient) {
         });
       };
 
-      $scope.isMe = function (task) {
+      $scope.isMe = function (msg) {
         console.log(Meteor.userId());
-        return task.owner === Meteor.userId();
+        return msg.owner === Meteor.userId();
       }
  
     }]);
@@ -35,37 +35,21 @@ if (Meteor.isClient) {
   angular.module('chat').config(themeIcons);
 
   function themeIcons ($mdIconProvider) {
+    $mdIconProvider
+      .iconSet("social", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg")
+      .iconSet("action", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg")
+      .iconSet("communication", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg")
+      .iconSet("content", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg")
+      .iconSet("toggle", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle.svg")
+      .iconSet("navigation", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg")
+      .iconSet("image", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg");
 
-  $mdIconProvider
-    .iconSet("social", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg")
-    .iconSet("action", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg")
-    .iconSet("communication", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg")
-    .iconSet("content", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg")
-    .iconSet("toggle", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle.svg")
-    .iconSet("navigation", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg")
-    .iconSet("image", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg");
-
-    // there are more icon sprites available, check out the material-design-icons page
-
-};
-}
+      // there are more icon sprites available, check out the material-design-icons page
+    };
+  }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-
-    if (Messages.find().count() === 0) {
-
-      var fake_messages = [
-        {'text': 'Come to the dark side!', createdAt: new Date(), owner: 'Cgc56SntriLN4GLFG', username: 'Vader'},
-        {'text': 'Whats the frequency', createdAt: new Date(), owner: 'r76kZTopyp2HPpcpw', username: 'sca'},
-        {'text': 'I have been waiting for you!', createdAt: new Date(), owner: 'Cgc56SntriLN4GLFG', username: 'Vader'}
-      ];
-
-      for (var i = 0; i < fake_messages.length; i++)
-        Messages.insert(fake_messages[i]);
-
-    }
-
   });
 }
