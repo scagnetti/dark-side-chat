7783848ce71040c76547f506b608bdbc4adaae35
scagnetti <scagnetti@gmx.net>
2015-08-09 01:29:11 +0200

    Replaced default login forms with material design

--- chat.js
+++ chat.js
@@ -2,13 +2,50 @@ Messages = new Mongo.Collection('messages');
  
 // This code only runs on the client
 if (Meteor.isClient) {
- 
-  Accounts.ui.config({
-    passwordSignupFields: "USERNAME_ONLY"
-  });
 
-  angular.module('chat',['angular-meteor', 'ngMaterial']);
+  angular.module('chat',['angular-meteor', 'ngMaterial', 'ui.router']);
  
+  angular.module('chat').controller('RegistrationCtrl', ['$scope', '$state',
+    function($scope, $state){
+
+      $scope.register = function() {
+        Accounts.createUser({
+          username: $scope.username,
+          password: $scope.password
+        }, function(error){
+            if(error){
+                console.log(error.reason);
+                $scope.failure = true;
+                $scope.reason = error.reason;
+            } else {
+                $state.go('chat')
+            }
+        });
+      };
+  }]);
+
+  angular.module('chat').controller('LoginCtrl', ['$scope', '$state',
+    function($scope, $state){
+
+      $scope.login = function() {
+        Meteor.loginWithPassword($scope.username, $scope.password, function(error){
+          if(error){
+            console.log(error.reason);
+            $scope.failure = true;
+            $scope.username = '';
+            $scope.password = '';
+          } else {
+            $state.go('chat');
+          }
+        });
+      };
+  }]);
+
+  angular.module('chat').controller('LogoutCtrl', ['$scope',
+    function($scope){
+      Meteor.logout();
+  }]);
+
   angular.module('chat').controller('ChatCtrl', ['$scope', '$meteor',
     function ($scope, $meteor) {
  
@@ -29,24 +66,54 @@ if (Meteor.isClient) {
         console.log(Meteor.userId());
         return msg.owner === Meteor.userId();
       }
- 
     }]);
 
-  angular.module('chat').config(themeIcons);
+  angular.module('chat').run(["$rootScope", "$state", function($rootScope, $state) {
+    $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+      // We can catch the error thrown when the $requireUser promise is rejected
+      // and redirect the user back to the main page
+      if (error === "AUTH_REQUIRED") {
+        $state.go('login');
+      }
+    });
+  }]);
 
-  function themeIcons ($mdIconProvider) {
-    $mdIconProvider
-      .iconSet("social", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg")
-      .iconSet("action", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg")
-      .iconSet("communication", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg")
-      .iconSet("content", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg")
-      .iconSet("toggle", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle.svg")
-      .iconSet("navigation", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg")
-      .iconSet("image", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg");
+  angular.module('chat').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
+    function($urlRouterProvider, $stateProvider, $locationProvider){
 
-      // there are more icon sprites available, check out the material-design-icons page
-    };
-  }
+      $locationProvider.html5Mode(true);
+
+      $stateProvider
+        .state('chat', {
+          url: '/chat',
+          templateUrl: 'chat.ng.html',
+          controller: 'ChatCtrl',
+          resolve: {
+            "currentUser": ["$meteor", function($meteor){
+              return $meteor.requireUser();
+            }]
+          }
+        })
+        .state('register', {
+          url: '/register',
+          templateUrl: 'registration.ng.html',
+          controller: 'RegistrationCtrl'
+        })
+        .state('login', {
+          url: '/login',
+          templateUrl: 'login.ng.html',
+          controller: 'LoginCtrl'
+        })
+        .state('logout', {
+          url: '/logout',
+          templateUrl: 'logout.ng.html',
+          controller: 'LogoutCtrl'
+        });
+
+        $urlRouterProvider.otherwise('/login');
+  }]);
+
+}
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
